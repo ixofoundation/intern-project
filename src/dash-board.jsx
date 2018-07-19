f3bd676419ab7f5653b72da10e79097186a33d20
Jacob Fullerton <fullertonj44@gmail.com>
2018-07-19 16:13:20 +0200

    change css

--- src/dash-board.jsx
+++ src/dash-board.jsx
@@ -4,12 +4,14 @@ import Launchbutton from './launch-button';
 import Web3 from 'web3';
 import swal from 'sweetalert';
 import utf8 from 'utf8';
-import base64 from 'base-64'
+import base64 from 'base-64';
+import ixologo from './ixologo.png'
+// import 'typeface-roboto'; 
 import { ENGINE_METHOD_DIGESTS } from 'constants';
 
 
 export default class Dashboard extends React.Component {
-
+  // styling properties 
   constructor(props) {
     super(props);
 
@@ -139,8 +141,7 @@ export default class Dashboard extends React.Component {
       return;
     }
     const blockchainProvider = (providerId === this.blockchainProviders.metamask.id) ? this.blockchainProviders.metamask : this.blockchainProviders.ixo_keysafe;
-    var PDSURLs = ["http://beta.elysian.ixo.world:5000/"]
-    this.uploadAndInsert(PDSURLs[0], this.state.messageBody, blockchainProvider);
+    this.uploadAndInsert("http://localhost:5000/", this.state.messageBody, blockchainProvider);
   }
   handleSchemaButtonClicked(providerId) {
     if (this.state.messageBody2.length === 0) {
@@ -187,43 +188,6 @@ export default class Dashboard extends React.Component {
     return encoded;
   }
 
-  // upload documents to pds
-  // text: encoded json for claim or schema
-  // message: project json string
-  // type: string that reads either "form" or "schema"
-  // 
-  // uploadAndInsert(text, PDSURL, message, type) {
-  //   let dataUrl = 'data:application/json;base64, ' + text;
-  //   var hash = this.state.ixo.project.createPublic(dataUrl, PDSURL).then((result) => {
-  //     // check output, it should now output the hash   
-  //     console.log("Result: " + JSON.stringify(result));
-
-  //     // display the "templates" property of project json
-  //     var projectJSON = JSON.parse(message);
-  //     console.log("Project JSON templates section" + JSON.stringify(projectJSON['templates']));
-
-  //     // if the type is schema, insert hash into "schema" section of project json 
-  //     if (type == "schema") {
-  //       console.log("schema hash: " + JSON.stringify(result.result));
-  //       projectJSON['templates']['schema'] = JSON.stringify(result.result);
-  //       this.state.messageBody = projectJSON; 
-
-  //     }
-  //     if (type == "form") {
-  //       console.log("form hash: " + JSON.stringify(result.result));
-  //       projectJSON['templates']['form'] = JSON.stringify(result.result);
-  //       // update the state of messageBody to reflect new addition
-  //       this.state.messageBody = projectJSON;
-
-  //     }
-  //     // check to see if templates were inserted into project json 
-  //     console.log("Project JSON templates section after additions: " + JSON.stringify(projectJSON['templates']));
-
-  //   }).catch((error) => {
-  //     console.log("Error, unable to return hash");
-  //     console.log(error);
-  //   });
-  // }
 
   uploadAndInsert(PDSURL, message, blockchainProvider) {
     var encodedClaimSchema = this.encodeJSON(this.state.messageBody2);
@@ -266,8 +230,8 @@ export default class Dashboard extends React.Component {
               this.state.ixo.project.createProject(projectJSON, response, PDSURL).then((result) => {              console.log(`Project Details:   \n${JSON.stringify(result)}`)
                 swal({
                   title: 'Your project has been created!',
-                  text: 'Click OK to be redirected to ixo.world and get started!',
-                  icon: "success"
+                  text: 'You can find your new project on the ixo website with other current projects. \n \n Click OK to be redirected to the ixo website',
+                  type: "success"
                 })
                   .then(redirect => {
                     if (redirect) {
@@ -277,7 +241,7 @@ export default class Dashboard extends React.Component {
               })
             } catch (error) {
               console.log("Incorrect PDS URL format")
-              swal("ERROR","Please make sure that you have pasted everything exactly as it appears in the email.", "error")
+              swal("ERROR", "Incorrect PDS URL format", "error")
             }
   
           })
@@ -295,65 +259,15 @@ export default class Dashboard extends React.Component {
         }
       }).catch((error) => {
         console.log("Error, unable to return form");
-        swal("ERROR","Please make sure that you have pasted everything exactly as it appears in the email.", "error")
         console.log(error);
       });
 
    
     }).catch((error) => {
       console.log("Error, unable to return schema hash");
-      swal("ERROR","Please make sure that you have pasted everything exactly as it appears in the email.", "error")
       console.log(error);
     });
   }
-
-  // have user sign and upload their project
-  // signMessageWithProvider(message, blockchainProvider, PDSURL) {
-  //   // encode the claim schema and claim form json strings and upload them to pds
-  //   var encodedClaimSchema = this.encodeJSON(this.state.messageBody2);
-  //   console.log("Encoded schema json: " + encodedClaimSchema);
-  //   var encodedClaimForm = this.encodeJSON(this.state.messageBody3);
-  //   console.log("Encoded form json: " + encodedClaimForm);
-  //   var hashes = this.uploadAndInsert(encodedClaimSchema, encodedClaimForm, PDSURL, this.state.messageBody, "schema");
-
-
-  //   if (blockchainProvider.id === this.blockchainProviders.ixo_keysafe.id) {
-  //     this.blockchainProviders.ixo_keysafe.provider.requestSigning(message, (error, response) => {
-  //       //alert(`Dashboard handling received response for SIGN response: ${JSON.stringify(response)}, error: ${JSON.stringify(error)}`)
-  //       console.log(`Dashboard handling received response for SIGN response: \n${JSON.stringify(response)}\n, error: \n${JSON.stringify(error)}\n`)
-  //       try {
-  //         this.state.ixo.project.createProject(JSON.parse(message), response, PDSURL).then((result) => {
-  //           console.log(`Project Details:   \n${JSON.stringify(result)}`)
-  //           swal({
-  //             title: 'Your project has been created!',
-  //             text: 'You can find your new project on the ixo website with other current projects. \n \n Click OK to be redirected to the ixo website',
-  //             type: "success"
-  //           })
-  //             .then(redirect => {
-  //               if (redirect) {
-  //                 window.location.href = 'https://ixo.foundation/';
-  //               }
-  //             });
-  //         })
-  //       } catch (error) {
-  //         console.log("Incorrect PDS URL format")
-  //         swal("ERROR", "Incorrect PDS URL format", "error")
-  //       }
-
-  //     })
-  //     return
-  //   } else {
-  //     this.getEthereumAddressAsync().then(address => {
-  //       console.log(`${blockchainProvider.extension} -> Address: ${address}`);
-
-  //       // actual signing ->>
-  //       var dataInHex = '0x' + new Buffer(message).toString('hex');
-
-  //       blockchainProvider.provider.eth.personal.sign(dataInHex, address, "test password!")
-  //         .then(console.log);
-  //     });
-  //   }
-  // }
 
   getEthereumAddressAsync() {
     const eth = this.blockchainProviders.metamask.provider.eth;
@@ -368,21 +282,34 @@ export default class Dashboard extends React.Component {
     });
   }
 
+  print() {
+    console.log("HAAAAAAAA");
+  }
 
   render() {
     return (
-      <div>
-        {/* {this.blockchainProviders.ixo_keysafe.doShow && 
-          <button onClick={this.handleSimulateDidDocLedgeringButtonClicked}>Ledger DID Manually</button>
-        }
-        {this.blockchainProviders.ixo_keysafe.doShow && 
-          <button onClick={this.handleRequestInfoButtonClicked}>ixo INFO</button>
-        } */}
-        {/* <div></div>
+      <div class='background'>
+        <div className="topRect">
+          <img id='logo-top' src={ixologo} alt='logo' />
+          <h3>EXPLORE</h3>
+        </div>
+        <div className="secondHeader">
+          <h1>Create Project</h1>
+          <p id='subtitle'> Upload your project for the world to see.</p>
+          <div></div>
+        </div>
+
+
         <br></br>
+        {/* <input value={this.state.messageBody2} onChange={this.handleMessageBodyChanged2} /> */}
+        {/* <Launchbutton
+          provider={this.blockchainProviders.ixo_keysafe.id}
+          title="Sign Schema"
+          handleLaunchEvent={this.handleSchemaButtonClicked} />
         <br></br>
-        <br></br> */}
-        <br></br>
+        Paste the Schema into the above textbox to Sign
+
+      <br></br>
       <b>1. Paste the Schema text here:</b>
       <br></br>
       <textarea style={{ height: 100, width: 400 }}
@@ -399,7 +326,8 @@ export default class Dashboard extends React.Component {
       <br></br>
       <br></br>
       <br></br>
-      <b>3. Paste the Project Details here:</b><br></br>
+      <b>3. Paste the Project Details here</b><br></br><b>to sign and create your project:</b>
+      <br></br>
       <textarea style={{ height: 100, width: 400 }}
         value={this.state.messageBody} 
         onChange={this.handleMessageBodyChanged} /> 
@@ -409,10 +337,32 @@ export default class Dashboard extends React.Component {
           provider={this.blockchainProviders.ixo_keysafe.id}
           title="Sign and Create Project"
           handleLaunchEvent={this.handleExtensionLaunch} />
-        
-        {this.blockchainProviders.ixo_keysafe.doShow}
+        {this.blockchainProviders.ixo_keysafe.doShow} */}
+        <div class="mask">
+          <h2>Almost there!</h2>
+          <div class="line-2">
+          </div>
+          <h6>To create your project, click the button below and enter your ixo keysafe
+          password in the window that appears.
+          </h6>
+          <div class="button-box">
+            <div class="button" onClick={this.print}>
+              <p id='button-text' onClick={this.print()}>+&emsp;Create Project</p>
+            </div>
+          </div>
+        </div>
 
+        <div class='footer'>
+          <img id='logo-bottom' src={ixologo} alt='logo' />
+          <p>About</p>
+          <p>Membership</p>
+          <p>Ecosystem</p>
+          <p>Network</p>
+          <p>Plans/ Pricing</p>
+          <p>Support</p>
+        </div>
       </div>
     )
   }
 }
+
